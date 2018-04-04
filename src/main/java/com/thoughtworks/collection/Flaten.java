52f4d8b1eb8475f41fcca7c2fe57736e3825b6a4
hhy <1014596312@qq.com>
2018-04-04 13:46:12 +0800

    [Flaten] 完成

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,31 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Stream.of(array)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return Stream.of(array)
+                .flatMap(Stream::of)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
